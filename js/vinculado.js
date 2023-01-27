51b8fcb0759e787c399ad31b7c225e99eef77929
TobiasElMagnifico <tobiaselmagnifico@gmail.com>
2023-01-27 16:31:12 -0300

    Este es mi segundo commit del CV

---
+++ js/vinculado.js
@@ -0,0 +1,24 @@
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+    btnSwitch.classList.toggle('active');
+
+
+    if(document.body.classList.contains('light')){
+        localStorage.setItem('light-mode', 'true');
+
+    } else {
+        localStorage.setItem('light-mode', 'false');
+    }
+});
+
+if (localStorage.getItem('light-mode') === 'true'){
+    document.body.classList.add('light');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('light');
+    btnSwitch.classList.remove('active');
+}
